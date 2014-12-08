ab52a7f7e1bd9926e45d79f821aed5a09fcad2ee
niklaus <niklaus.lee@gmail.com>
2014-12-08 20:08:17 +0900

    added all diagrams and index pages

--- ejs_test.js
+++ ejs_test.js
@@ -5,8 +5,8 @@ var fs  = require("fs-extra");
 var mdj = require("./metadata-json");
 var gen = require("./lib/generator");
 
-var project = mdj.loadFromFile("test/diagram_pdf_test.mdj");
-// var project = mdj.loadFromFile("test/StarUML2.mdj");
+// var project = mdj.loadFromFile("test/diagram_pdf_test.mdj");
+var project = mdj.loadFromFile("test/StarUML2.mdj");
 
 
 
@@ -28,9 +28,16 @@ var options = {
 
 gen.render("html/templates/index.ejs", targetDir + "/index.html", options);
 gen.render("html/templates/navigation.ejs", targetDir + "/contents/navigation.html", options);
+gen.render("html/templates/diagrams.ejs", targetDir + "/contents/diagrams.html", options);
+gen.render("html/templates/element_index.ejs", targetDir + "/contents/element_index.html", options);
 
+// for Project
+options.element = project;
+gen.render("html/templates/content.ejs", targetDir + "/contents/home.html", options);
+
+// for Elements
 project.traverse(function (element) {
-    if (element instanceof type.Model) {
+    if (!(element instanceof type.Project) && element instanceof type.Model) {
         options.element = element;
         gen.render("html/templates/content.ejs", targetDir + "/contents/" + gen.toFilename(element)  + ".html", options);
     }
